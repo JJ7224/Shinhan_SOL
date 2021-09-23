9dcbafb91514908d6053d43c2c2294143196b37e
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-09-23 23:58:06 +0900

    ADD: OTID xml

---
+++ app/src/main/java/com/example/shinhan_sol/OTIDActivity.kt
@@ -0,0 +1,11 @@
+package com.example.shinhan_sol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OTIDActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otidactivity)
+    }
+}
