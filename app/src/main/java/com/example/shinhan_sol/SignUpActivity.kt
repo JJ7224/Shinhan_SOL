3f1367e9c80f3f2dd6025c39c65d9ca307c92dee
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-10-07 15:26:49 +0900

    UPDATE: Signup xml

--- app/src/main/java/com/example/shinhan_sol/SignUpActivity.kt
+++ app/src/main/java/com/example/shinhan_sol/SignUpActivity.kt
@@ -1,11 +1,48 @@
 package com.example.shinhan_sol
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioGroup
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var userId: EditText
+    lateinit var userPw: EditText
+    lateinit var userPwCheck: EditText
+    lateinit var userEmail: EditText
+    //중간에 주민등록번호 넣어야함
+    lateinit var userName: EditText
+    lateinit var userNickName: EditText
+    lateinit var btnRegisterPhoto: Button
+    lateinit var checkAgree: CheckBox
+    lateinit var btnSignup: Button
+    lateinit var btnOverlap: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+    }
+
+    fun signup() {
+
+    }
+
+    fun init(activity: Activity) {
+        userId = activity.findViewById(R.id.edit_id)
+        userPw = activity.findViewById(R.id.edit_pw)
+        userPwCheck = activity.findViewById(R.id.edit_check_pw)
+        userEmail = activity.findViewById(R.id.edit_email)
+        userName = activity.findViewById(R.id.edit_name)
+        //중간에 주민등록번호 넣어야함
+        userNickName = activity.findViewById(R.id.edit_nick_name)
+        btnRegisterPhoto = activity.findViewById(R.id.btn_register_photo)
+        checkAgree = activity.findViewById(R.id.check_agree)
+        btnSignup = activity.findViewById(R.id.btn_signup)
+        btnOverlap = activity.findViewById(R.id.btn_overlap)
     }
 }
