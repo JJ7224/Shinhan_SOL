ba9fbe73937a4d9a0bc54d6326c0fd54ec612f40
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-10-21 20:42:49 +0900

    ADD: Retrofit

--- app/src/main/java/com/example/shinhan_sol/RetrofitService.kt
+++ app/src/main/java/com/example/shinhan_sol/RetrofitService.kt
@@ -1,11 +1,9 @@
 package com.example.shinhan_sol
 
-import okhttp3.MediaType
-import okhttp3.MultipartBody
+import com.example.shinhan_sol.data_type.Login
+import com.example.shinhan_sol.data_type.SignUp
 import okhttp3.RequestBody
 import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.http.*
 
 
@@ -16,10 +14,20 @@ interface RetrofitService {
         @Body login: Login
     ): Call<Any?>
 
+    @POST("/login/quick")
+    fun loginOTID(
+        @Query("quickpw") quickpw: String
+    ): Call<Any?>
+
     @Multipart
     @POST("/signup")
     fun signup(
         @Part image: SignUp,
         @PartMap data: HashMap<String, RequestBody>
+    ): Call<Any?>
+
+    @POST("/signup/quick")
+    fun signupOTID(
+        @Query("quickpw") quickpw: String
     ): Call<Any?>
 }
