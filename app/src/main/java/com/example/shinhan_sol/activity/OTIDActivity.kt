72120711d0edcd1a49827a88679a5031c9baaebc
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-10-28 20:47:10 +0900

    FIX: OTIDActivity

--- app/src/main/java/com/example/shinhan_sol/activity/OTIDActivity.kt
+++ app/src/main/java/com/example/shinhan_sol/activity/OTIDActivity.kt
@@ -1,9 +1,11 @@
 package com.example.shinhan_sol.activity
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputFilter
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -17,25 +19,26 @@ import java.util.regex.Pattern
 class OTIDActivity : AppCompatActivity() {
 
     lateinit var userOTID: EditText
-    lateinit var btnOTID: Button
-
-    init {
-        userOTID = findViewById(R.id.edit_OTID)
-        btnOTID = findViewById(R.id.btn_OTID)
-    }
+    private lateinit var btnOTID: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otidactivity)
 
+        init(this@OTIDActivity)
 
+        btnOTID.setOnClickListener {
+            OTIDsignup(this@OTIDActivity)
+        }
     }
 
-    fun OTIDsignup() {
+    fun OTIDsignup(activity : Activity) {
         val userOTID = userOTID.text.toString()
         (application as MasterRetrofit).service.signupOTID(userOTID)
             .enqueue(object : Callback<Any?> {
                 override fun onResponse(call: Call<Any?>, response: Response<Any?>) {
+
+                    Log.d("retrofittCode","$response.code()")
                     if (response.isSuccessful) {
                         Toast.makeText(this@OTIDActivity, "간편인증번호가 설정되었습니다.", Toast.LENGTH_SHORT)
                             .show()
@@ -46,13 +49,20 @@ class OTIDActivity : AppCompatActivity() {
 
                 override fun onFailure(call: Call<Any?>, t: Throwable) {
                     if (userOTID.toString().isEmpty()) {
-                        Toast.makeText(this@OTIDActivity, "입력 칸을 다 채워주세요.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@OTIDActivity, "입력 칸을 다 채워주세요.",
+                            Toast.LENGTH_SHORT).show()
                     }
-//                    else if () {
-//
-//                    }
+                    else {
+                        Toast.makeText(this@OTIDActivity, "입력 정보가 잘못되었습니다",
+                            Toast.LENGTH_SHORT).show()
+                    }
                 }
             })
+    }
+
+    fun init(activity: Activity) {
+        userOTID = activity.findViewById(R.id.edit_OTID)
+        btnOTID = activity.findViewById(R.id.btn_OTID)
     }
 
 }
