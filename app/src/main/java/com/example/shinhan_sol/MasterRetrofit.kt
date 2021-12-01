7d89a7df308dfa359816d0d02b9897ed802ec931
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-12-01 19:14:00 +0900

    FIX: MasterRetrofit.kt

--- app/src/main/java/com/example/shinhan_sol/MasterRetrofit.kt
+++ app/src/main/java/com/example/shinhan_sol/MasterRetrofit.kt
@@ -8,6 +8,31 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MasterRetrofit : Application() {
+class MasterRetrofit {
 
+    fun TokenRetrofit(): RetrofitService {
+
+        val okHttpClient = OkHttpClient.Builder().addInterceptor(AuthInterceptor()).build()
+
+        val retrofit: Retrofit = Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl("http://192.168.0.21:8040/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(RetrofitService::class.java)
+
+        return service
+    }
+
+    fun Retrofit(): RetrofitService {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("http://192.168.0.21:8040/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(RetrofitService::class.java)
+
+        return service
+    }
 }
