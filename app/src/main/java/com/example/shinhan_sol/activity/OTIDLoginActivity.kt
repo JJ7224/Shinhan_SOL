7977088961cc9c78968eba837841a2fac83a4c35
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-12-04 23:13:43 +0900

    ADD: OTIDLoginActivity.kt

---
+++ app/src/main/java/com/example/shinhan_sol/activity/OTIDLoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shinhan_sol.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shinhan_sol.R
+
+class OTIDLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otidlogin)
+    }
+}
